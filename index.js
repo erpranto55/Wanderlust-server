f92b954da979d7408f3349173cdd50e0e6259db8
erpranto <erpranto55@gmail.com>
2026-05-25 12:33:27 +0600

    Git ignore updated

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
 
-const PORT = 5000;
+const uri = process.env.MONGODB_URI;
+const app = express();
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+
+
+
+
+    
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running fine!");
