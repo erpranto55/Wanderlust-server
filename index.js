2c2bced90fa839b5b291761f4ccc68c97bc1b7ca
erpranto <erpranto55@gmail.com>
2026-05-25 12:52:01 +0600

    Created Post api

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -17,18 +21,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-
-
-
-    
     await client.connect();
+
+    const db = client.db("wanderlust");
+
+    const destinationCollection = db.collection("destinations");
+
+    app.post("/destination", async (req, res) => {
+      const destinationData = req.body;
+      console.log(destinationData);
+      const result = await destinationCollection.insertOne(destinationData);
+
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
